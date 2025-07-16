078d280d4744075bcd35b40feeac93e0b4cbfd2d
Dewy <dewygame1@gmail.com>
2025-07-16 10:03:27 +0700

    api

--- lib/util/api_config.dart
+++ lib/util/api_config.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class ApiConfig {
   static const ApiConfig local = ApiConfig._(
     // endpoint: "http://172.20.10.13:85/api",
-    endpoint: "http://192.168.1.10:84/api",
+    // endpoint: "http://192.168.1.10:84/api",
+    endpoint: "http://27.254.253.176:82/api",
     endpointDownloadApk: "https://bas-zeus.ftcs.in.th:86/api",
     endpointName: "Local",
     colors: [
